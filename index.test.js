1a6da57d724d081e5d1abd99c26face264127a3d
Alex Vlasov <superkoder@gmail.com>
2025-03-18 17:04:07 +0300

    feat: esbuild

--- index.test.js
+++ index.test.js
@@ -1,24 +1,23 @@
 import test from 'ava';
-import alfyTest from 'alfy-test';
+import { testRun } from './index.js'
 
-const alfy = alfyTest();
 
 const cases = [
-	['1h + 30m', '1 hour, 30 minutes'],
-	['20d * 2', '40 days'],
-	['5m / 2', '2 minutes, 30 seconds'],
-	['1h - 5s', '59 minutes, 55 seconds'],
-	['300ms * 4', '1.2 seconds'],
+    ['1h + 30m', '1 hour, 30 minutes'],
+    ['20d * 2', '40 days'],
+    ['5m / 2', '2 minutes, 30 seconds'],
+    ['1h - 5s', '59 minutes, 55 seconds'],
+    ['300ms * 4', '1.2 seconds'],
 ];
 
 for (const [input, output] of cases) {
-	test(`success: ${input} → ${output}`, async t => {
-		const result = await alfy(input);
-		t.deepEqual(result, [{title: output, subtitle: 'Press Enter to copy', arg: output}]);
-	});
+    test(`success: ${input} → ${output}`, async t => {
+        const result = JSON.parse(testRun(input)).items;
+        t.deepEqual(result, [{title: output, subtitle: 'Press Enter to copy', arg: output}]);
+    });
 }
 
 test('error', async t => {
-	const [item] = await alfy('1test');
-	t.is(item.title, 'Invalid input');
+    const [item] = JSON.parse(testRun("1test")).items;
+    t.is(item.title, 'Invalid input');
 });
