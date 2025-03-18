b5f102c5b5925fe722b8b2f779dca7076a23ef9c
Alex Vlasov <superkoder@gmail.com>
2025-03-18 23:08:28 +0300

    chore: console

--- main.js
+++ main.js
@@ -158,7 +158,6 @@ var formatSeconds = (x) => new Intl.NumberFormat("en-US", {
   maximumFractionDigits: 3
 }).format(x);
 function humanizeDuration(seconds) {
-  console.log(seconds);
   if (Number.isNaN(seconds)) {
     throw new TypeError("second argument must be a number");
   }
@@ -166,7 +165,12 @@ function humanizeDuration(seconds) {
   const h = Math.floor(seconds % 86400 / 3600);
   const m = Math.floor(seconds % 3600 / 60);
   const s = seconds % 60;
-  return [d > 0 ? `${d} day${d > 1 ? "s" : ""}` : "", h > 0 ? `${h} hour${h > 1 ? "s" : ""}` : "", m > 0 ? `${m} minute${m > 1 ? "s" : ""}` : "", s === 0 ? "" : `${formatSeconds(s)} second${s > 1 ? "s" : ""}`].filter(Boolean).join(", ");
+  return [
+    d > 0 ? `${d} day${d > 1 ? "s" : ""}` : "",
+    h > 0 ? `${h} hour${h > 1 ? "s" : ""}` : "",
+    m > 0 ? `${m} minute${m > 1 ? "s" : ""}` : "",
+    s === 0 ? "" : `${formatSeconds(s)} second${s > 1 ? "s" : ""}`
+  ].filter(Boolean).join(", ");
 }
 function calculate(value) {
   const expression = value.toLowerCase().replaceAll(/(\d+\.?\d*[a-z]+)/g, (match) => {
