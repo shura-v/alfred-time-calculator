f82d80b537b84d4a6670090460aac1029f28f229
Alexander Vlasov <superkoder@gmail.com>
2025-04-09 22:12:08 +0300

    refactor: minor

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,17 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      exclude: [
-        "vitest.config.ts",
-        "build/**",
-        "dist/**",
-        "dist-workflow/**",
-        "src/index.ts",
-        "src/cli.ts",
-        "src/types.ts",
-        "src/workflow.ts",
-        "src/calculate/types.ts",
-      ],
+      include: ["src/calculate/**"],
+      exclude: ["src/calculate/types.ts"],
     },
   },
 });
